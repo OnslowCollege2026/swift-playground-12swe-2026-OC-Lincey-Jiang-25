1455a27b5cbc81089029a3ae9ad34719d56374d3
OC-Lincey-Jiang-25 <lincey.jiang@student.onslow.school.nz>
2026-02-13 01:18:45 +0000

    Completed Function documentation task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,42 +2,51 @@
 // https://docs.swift.org/swift-book
 
 // Task 1.
-func roomVolume() -> Double {
-    return  5 * 3 * 4
-}
-
-// Task 2.
-func printArea(length: Double, width: Double) {
-    let area = length * width
-    print("The area of the room is \(area) m²")
-}
-
-// Task 3.
-func area(length: Double, width: Double) -> Double {
+/* Calculates the area of the room.
+    - Parameters:
+        length: The length of the room in meters.
+        width: The width of the room in meters.
+    - Returns: The area of the room in square meters.
+*/
+func roomArea(length: Double, width: Double) -> Double {
     return length * width
 }
 
-// Task 4.
-func volume(length: Double, width: Double, height: Double) -> Double {
+// Task 2.
+/* Calculates the volume of the room.
+        - Parameters:
+            length: The length of the room in meters.
+            width: The width of the room in meters.
+            height: The height of the room in meters.
+        - Returns: The volume of the room in cubic meters.
+*/
+func roomVolume(length: Double, width: Double, height: Double) -> Double {
     return length * width * height
+}
+
+// Task 3.
+/* Checks if the room is small.
+    - Parameter volume: the volume of the room in cubic meters.
+    - Returns: True if the room volume is less than 60 cubic meters.
+    */
+func isRoomSmall(volume: Double) -> Bool {
+    return volume < 60
+}
+// Task 4.
+/* Calculates the new height.
+    - Parameters:
+        original: The original height of the room in meters.
+        reductionPercent: The pecentage the height is shrinking.
+    - Returns: The new and reduced height of the room in meters.
+
+*/
+func reducedHeight(original: Double, reductionPercent: Double) -> Double {
+    let multiplier = (100.0 - reductionPercent) / 100.0
+    return original * multiplier
 }
 
 @main
 struct SwiftPlayground {
     static func main(){
-        // Task 1
-        print("The volume of the room is", roomVolume(), "m³")
-
-        // Task 2.
-        printArea(length: 5, width: 3)
-
-        // Task 3.
-        print(area(length: 3.2, width: 4.5), "m²")
-
-        // Task 4
-        print(volume(length: 3.2, width: 4.5, height: 6), "m³")
-        }
-
-        
     }   
-
+}
