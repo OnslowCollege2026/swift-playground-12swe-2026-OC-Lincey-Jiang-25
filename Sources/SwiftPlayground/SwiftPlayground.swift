cd9929590c504197e62d9be899d61d8e1391fb72
OC-Lincey-Jiang-25 <lincey.jiang@student.onslow.school.nz>
2026-03-24 00:16:14 +0000

    duolingo

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,83 +1,73 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func readNumber(prompt: String) -> Double {
-print(prompt)
-        var returnValue = -1.0
-        guard let inputLength = readLine(), let number = Double(inputLength), number > 0 else {
-            print("Invalid Number")
-            return readNumber(prompt: prompt)
-        }
-        returnValue = number
-        return returnValue
-}
 
 @main
 struct SwiftPlayground {
     static func main(){
-        // Lists the individual volumes of furniture.
-        let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
+        // Constants and variables.
 
-        // Sets a maximum volume for a singular piece of furniture.
-        let maxFurnitureVolume = 2.0
+        // Each inner array contains the english word, the correct anser, and then 3 incorrect answers
+        let vocabulary = [
+            ["Hello", "Hallo", "Holla", "Helo", "bonjour"],
+            ["Goodbye", "Auf Wiedersehen", "" ],
+            ["Yes", "Ja", "Ya", "Lampe", "Dunkel" ],
+            ["No", "Nein", "Nah", "Tag", "Katze"],
+            ["Good Evening", "Guten Abend", "Guten Tag", "Guten Morgen", "Hund" ]
+        ]
 
-        // Stores the volume of all the furniture that meets the volume requirement. 
-        var totalFurnitureVolume: [Double] = []
+        // The indices of the questions that the user got wrong.
+        var incorrectIndices: [Int] = []
 
-        // Stores the volume of the room.
-        var totalRoomVolume: [Double] = []
+        /// The number of questions that the user got wrong first time around.
+        var incorrectCount = 0
 
-        // Part 8
-        var height = -1.0
-        let minimumHeight = 1.0
+        // The number of questions that the user got right.
+        var correctIndicies: [Int] = []
+
+        /// The number of questions that the user got right first time around.
+            var count = 0
+
+        // Loop until all of the vocabulary questions have been asked.
+        // while count < vocabulary.count {
+
+        // Show the question.
+        for array in vocabulary {
+            print("What is the correct German word for \(array[0])")
         
-        // Part 1 (Room dimensions).
-        // Gets the length of the room from the user.
-        let roomLength = readNumber(prompt: "Enter the room Length: ")
+            // Present the possible answers.
+            let random1 = Int.random(in: 1...4)
+            var random2 = Int.random(in: 1...4)
+            while random2 == random1 {
+                random2 = Int.random(in: 1...4)
+            }
+            var random3 = Int.random(in: 1...4)
+            while random3 == random1 || random3 == random2 {
+                random3 = Int.random(in: 1...4)  
+            }
+            var random4 = Int.random(in: 1...4)
+            while random4 == random1 || random4 == random2 || random4 == random3 {
+                random4 = Int.random(in: 1...4) 
 
-        // Gets the width of the room from the user.
-        print("Enter room width")
-        guard let inputWidth = readLine(), let roomWidth = Double(inputWidth)  else { 
-            print("Invalid number")
-        }
-            
+            print(random1, random2, random3, random4)
 
-                // Gets the height of the room from the user.
-            print("Enter room height") 
-        guard let inputHeight = readLine(), let roomWidth = Double(inputHeight)  else { 
-            print("Invalid number")
-        }
-                // Part 2 (Room area).
-                let area = roomLength * roomWidth
-                print("The area of this room is\(area) m²")
-                // Part 4 (Room Volume).
-                let roomVolume = roomLength * roomWidth * roomHeight
-                print("The volume of the room is \(roomVolume)")
-                totalRoomVolume.append(roomVolume)
+            let option1: Int = random1
+            let option2: Int = random2
+            let option3: Int = random3
+            let option4: Int = random4
             
-            
-            
-            // Lists each piece of furnitures volume.
-            for (index, furnitureVolume) in furnitureVolumes.enumerated() {
+                print("""
+                1: \(array[option1])
+                2: \(array[option2])
+                3: \(array[option3])
+                4: \(array[option4])
+                """)
+            // Check if the user guessed the correct answers.
 
-                // Makes sure that the piece of furniture meets the Max Volume requirements.
-                if furnitureVolume < maxFurnitureVolume {
-                    print("Item \(index + 1): \(furnitureVolume)m³")
-                    totalFurnitureVolume.append(furnitureVolume)
-                } else {
-                    // Lets the user know if a piece of furniture is above the maximum volume limit.
-                    print("Item \(index + 1): Oversized item detected.")
-                }
-                }
+            // If not, make a note of the questions to ask again later.
 
-            let furniture = Double(totalFurnitureVolume.reduce(0, +))
-            let room = Double(totalRoomVolume.reduce(0, +))
-            let usableVolume = room - furniture
-            print("Total room area \(area) m²")
-            print("Total room Volume: \(roomVolume)m³")
-            print("Total usable volume: \(usableVolume)m³.")
-            
-        }}
-            
-        
+            }
+    }
+        }  
 
+}
